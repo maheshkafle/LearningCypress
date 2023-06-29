e0b85046c42b9a120539f1091742d6c964023068
MaheshKafle <mahesh.kafle@lisec.com>
2023-06-30 03:14:50 +0400

    Alert and Confirm Dialog handling along with assertions

---
+++ cypress/e2e/alert.cy.js
@@ -0,0 +1,18 @@
+describe('TestSuite ', () => {
+    it('Test case', () => {
+      cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+      cy.get("#name").type("Mahesh")
+      cy.get("#alertbtn").click()
+      cy.get("#confirmbtn").click()
+      //Assert the text present in Alert winwow
+      cy.on('window:alert', (str1)=> {
+        //chai assertion
+        expect(str1).to.eq("Hello Mahesh, share this practice page and share your knowledge")
+      })
+      //Assert the text present in Alert winwow
+      cy.on('window:confirm', (str2)=> {
+        //Chai assertion
+        expect(str2).to.eq("Hello , Are you sure you want to confirm?")
+      })
+  })
+})
