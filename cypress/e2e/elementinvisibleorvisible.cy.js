4e5560008fe06ccee173f554019239b70f6f62ea
MaheshKafle <mahesh.kafle@lisec.com>
2023-06-29 20:54:00 +0400

    Visible and Invisible Elements handling

---
+++ cypress/e2e/elementinvisibleorvisible.cy.js
@@ -0,0 +1,10 @@
+describe('TestSuite ', () => {
+    it('Test case', () => {
+      cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+      cy.get("#displayed-text").should("be.visible")
+      cy.get("#hide-textbox").click()
+      cy.get("#displayed-text").should("not.be.visible")
+      cy.get("#show-textbox").click()
+      cy.get("#displayed-text").should("be.visible")
+    })
+  })
