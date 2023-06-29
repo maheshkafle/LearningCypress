112c67e1b199e0e880ef087697a15bfae644e002
MaheshKafle <mahesh.kafle@lisec.com>
2023-06-29 19:16:14 +0400

    Static and Dynamic dropdown handling

---
+++ cypress/e2e/dropdown.cy.js
@@ -0,0 +1,18 @@
+describe('TestSuite ', () => {
+    it('Test case', () => {
+      cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+      
+      //Static dropdowns
+      cy.get('select').select("option1").should('have.value', 'option1')
+
+      //Dynamic dropdowns
+      cy.get("#autocomplete").type("united")
+      cy.get(".ui-menu-item div").each( ($el, index, $list) =>{
+        cy.log($el.text())
+        if($el.text()==="United States (USA)"){
+          $el.click()
+        }
+      })
+      cy.get("#autocomplete").should('have.value', 'United States (USA)')
+    })
+  })
