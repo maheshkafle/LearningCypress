7315b43c243709bb6681ba33303ff366696d746f
MaheshKafle <mahesh.kafle@lisec.com>
2023-07-01 02:29:54 +0400

    Handle invisible element using force:true

--- cypress/e2e/mousehover.cy.js
+++ cypress/e2e/mousehover.cy.js
@@ -1,9 +1,9 @@
 describe('TestSuite ', () => {
     it('Test case', () => {
       cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
-      cy.get("#mousehover").invoke('show')
-      cy.wait(700)
-      cy.contains("Top").click({force: true});
+    //   cy.get(".mouse-hover-content").invoke('show')  //exact parent element
+      cy.contains("Top").click({force: true}); //beauty of cypress without clicking any parent element , child element can be forced opened i.e handle invisible element using force:true
+    //   cy.contains("Top").click();
       cy.url().should('include','top')
     })
   })
